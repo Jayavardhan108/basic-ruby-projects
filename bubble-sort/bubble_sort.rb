88883d65f77e5f6a233198c22312823f3f8ff138
Jay <64061085+Jayavardhan108@users.noreply.github.com>
2023-03-04 08:07:20 +0530

    add bubble_sort.rb

---
+++ bubble-sort/bubble_sort.rb
@@ -0,0 +1,18 @@
+# lets go from bottom-up
+# while looping thru array swap elements when num1 > num2
+# repeat this loop for arr length times
+
+
+
+def bubble_sort array
+len = array.length
+while(len > 0)  
+  array[0..-2].each_with_index do |num1, index|
+    array[index], array[index + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
+  end
+  len -= 1
+end
+array
+end
+
+p bubble_sort [4,3,78,2,0,2]
