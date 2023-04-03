f5841d5b380b8ed3400c211f935ddd046d54e5fc
Jay <64061085+Jayavardhan108@users.noreply.github.com>
2023-04-03 08:31:41 +0530

    update balanced_bst

--- balanced_bst/tree.rb
+++ balanced_bst/tree.rb
@@ -55,6 +55,62 @@ class Tree
     node
   end
 
+  def delete(value, node)
+    if node.nil?
+      return node
+    end
+
+    if value < node.value
+      node.left = delete(value, node.left)
+    elsif value > node.value
+      node.right = delete(value, node.right)
+    else
+      if node.left.nil? || node.right.nil?
+        temp = nil
+        if temp == node.left
+          temp = node.right
+        elsif temp == node.right
+          temp = node.left
+        end
+
+        if temp.nil?
+          temp = node
+          node = nil
+        else
+          node = temp
+        end
+      else
+        temp = node.right
+        if !temp.left.nil?
+          temp = temp.left
+        end
+        node.value = temp.value
+        node.right = delete(temp.value, node.right)
+      end
+    end
+
+    unless node.nil?
+      balance_factor = balance_factor(node)
+
+      # left-left-case
+      if balance_factor > 1 && balance_factor(node.left) >= 0
+        node = right_rotate(node)
+      # left-right case
+      elsif balance_factor > 1 && balance_factor(node.left) < 0
+        node.left = left_rotate(node.left)
+        node = right_rotate(node)
+      # right-right case
+      elsif balance_factor < 1 && balance_factor(node.right) <= 0
+        node = left_rotate(node)
+      # right-left case
+      elsif balance_factor < 1 && balance_factor(node.right) > 0
+        node.right = right_rotate(node.right)
+        node = left_rotate(node)
+      end
+    end
+
+    node
+  end
   def find_height(node)
     if node.nil?
       return 0
@@ -84,7 +140,103 @@ class Tree
   end
 
   def balance_factor(node)
+    if node.nil?
+      return 0
+    end
     find_height(node.left) - find_height(node.right)
+  end
+
+  def find(value)
+    find_node(value, @root)
+  end
+
+  def find_node(value, root)
+    result_node = nil
+    if root.nil?
+      return result_node
+    end
+
+    if value == root.value
+      result_node = root
+    elsif value < root.value
+      result_node = find_node(value, root.left)
+    else
+      result_node = find_node(value, root.right)
+    end
+
+    result_node
+  end
+
+  def level_order
+    node = @root
+    queue = Array.new(1, node)
+
+    until queue.empty? || node.nil?
+      node = queue.pop
+      unless node.left.nil?
+        queue.prepend(node.left)
+      end
+
+      unless node.right.nil?
+        queue.prepend(node.right)
+      end
+
+      print "#{node.value} "
+    end
+
+  end
+
+  def preorder(root, block)
+    if root.nil?
+      return
+    end
+
+    unless block.nil?
+      block.call(root)
+      preorder(root.left, block)
+      preorder(root.right, block)
+    end
+    
+  end
+
+  def inorder(root, block)
+    if root.nil?
+      return
+    end
+
+    unless block.nil?
+      inorder(root.left, block)
+      block.call(root)
+      inorder(root.right, block)
+    end
+    
+  end
+
+  def postorder(root, block)
+    if root.nil?
+      return
+    end
+
+    unless block.nil?
+      postorder(root.left, block)
+      postorder(root.right, block)
+      block.call(root)
+    end
+    
+  end
+
+  def find_depth(value)
+    root_height = find_height(@root)
+    node = find_node(value, @root)
+    node_height = find_height(node)
+    root_height - node_height
+  end
+
+  def balanced? 
+    if balance_factor(@root) < 2
+      return true
+    end
+    false
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -111,4 +263,21 @@ tree.root = tree.insert(60, tree.root)
 tree.pretty_print
 puts tree.balance_factor(tree.root)
 tree.pretty_print
-
+tree.root = tree.delete(20, tree.root)
+tree.pretty_print
+tree.root = tree.delete(2, tree.root)
+tree.pretty_print
+tree.root = tree.delete(10, tree.root)
+tree.pretty_print
+p tree.find(4)
+tree.level_order
+puts
+block = ->(node) { print "#{node.value} " }
+tree.preorder(tree.root, block)
+puts
+tree.inorder(tree.root, block)
+puts
+tree.postorder(tree.root, block)
+puts
+puts "Depth of node 1 ::: #{tree.find_depth(1)}"
+puts "Is Tree Balanced? ::: #{tree.balanced?}"
